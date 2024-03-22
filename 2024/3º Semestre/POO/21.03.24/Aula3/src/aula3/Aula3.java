6e55dde632ec4e624d55a18a4a66e1fe277702ce
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2024-03-21 21:10:03 -0300

    ...

---
+++ 2024/3º Semestre/POO/21.03.24/Aula3/src/aula3/Aula3.java
@@ -0,0 +1,13 @@
+package aula3;
+/*
+ * @author antthLuca
+ */
+public class Aula3 {
+    public static void main(String[] args) {
+        Radio rl = new Radio("Philips", 0);
+        rl.aumentar_volume();
+        rl.getVolume();
+        rl.getEstacao();
+        rl.getMarca();
+    }
+}
