e5581a94d75ef7c9a228ce5db32abc564006fcba
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2024-07-12 22:13:48 -0300

    Aula 12.07.2024

---
+++ 2024/3º Semestre/POO/12.07.24/aula4/src/aula4/Aula4.java
@@ -0,0 +1,96 @@
+package aula4;
+import java.util.Scanner;
+import java.util.HashMap;
+/*
+ * @author antthLuca
+ */
+public class Aula4 {
+    public static void main(String[] args) {
+        System.out.println("---------------------- INÍCIO! ---------------------");
+        Ex1();
+        Divisor();
+        Ex2();
+        Divisor();
+        Ex3();
+        Divisor();
+        Ex4();
+        Divisor();
+        Ex5();
+        Divisor();
+        Ex6();
+        Divisor();
+        Ex7();
+        Divisor();
+        Ex8();
+        Divisor();
+        Ex9();
+        Divisor();
+        Ex10();
+        System.out.println("----------------------- FIM! -----------------------");
+    }
+    
+    public static void Divisor() {
+        System.out.println("");
+        System.out.println("----------------------------------------------------");
+        System.out.println("");
+    }
+    
+    public static void Ex1() {
+        int num1;
+        int num2;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o primeiro número: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Digite o segundo número: ");
+        num2 = sc.nextInt();
+        
+        
+        if (num1 == num2) {
+            System.out.println("Igual");
+        } else {
+            System.out.println("Diferente");
+        }
+    }
+    
+    
+    public static void Ex2() {
+        int a, b, c, d, e;
+        
+        System.out.println("");
+    }
+    
+    
+    public static void Ex3() {
+    }
+    
+    
+    public static void Ex4() {
+    }
+    
+    
+    public static void Ex5() {
+    }
+    
+    
+    public static void Ex6() {
+    }
+    
+    
+    public static void Ex7() {
+    }
+    
+    
+    public static void Ex8() {
+    }
+    
+    
+    public static void Ex9() {
+    }
+    
+    
+    public static void Ex10() {
+    }
+}
