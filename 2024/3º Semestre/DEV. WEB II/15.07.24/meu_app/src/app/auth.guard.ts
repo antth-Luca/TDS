eed7ce1b6ccbd71ec17f2e92bd077476f663cd4d
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2024-08-13 20:21:59 -0300

    Funcionamento total do Bootstrap 5.3.3!!!!!!!

--- 2024/3º Semestre/DEV. WEB II/15.07.24/meu_app/src/app/auth.guard.ts
+++ 2024/3º Semestre/DEV. WEB II/15.07.24/meu_app/src/app/auth.guard.ts
@@ -1,5 +1,26 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate{
+
+  constructor(private router: Router){
+
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  
+    const autenticacao = true; //== buscar de uma função
+    
+    if(autenticacao){
+      return true;
+    }else{
+      this.router.navigate(['/login']);
+      return false;
+    }
+  
+  }
+
+}
