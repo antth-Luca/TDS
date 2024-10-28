3e530d5cad8e38f4a1ec8c03987eff677739702a
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-10-28 10:15:37 -0300

    Remoção do espelho Django

--- 2024/4º semestre/Aula Django/app/Cidade/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Cidade(models.Model):
-    nome = models.CharField(max_length=100)
-    uf = models.CharField(max_length=2)
-
-    def __str__(self):
-        return f'{self.nome} - {self.uf}'
--- 2024/4º semestre/Aula Django/app/Cidade/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CreateCidadeView, ListCidadeView, EditCidadeView, DeleteCidadeView
-
-urlpatterns = [
-    path('list-cidade', ListCidadeView.as_view(), name='ListCidade'),
-    path('create-cidade', CreateCidadeView.as_view(), name='CreateCidade'),
-    path('edit-cidade/<int:pk>', EditCidadeView.as_view(), name='EditCidade'),
-    path('delete-cidade/<int:pk>', DeleteCidadeView.as_view(), name='DeleteCidade'),
-]
